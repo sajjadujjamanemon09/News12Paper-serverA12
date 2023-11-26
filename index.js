7980784f1ab3163d490b3bdbb3afe0ff7cce0f9b
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-26 18:41:09 +0600

    Recived article data form client side

--- index.js
+++ index.js
@@ -24,7 +24,70 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+
+    const articleCollection = client.db("newsPaper").collection("title");
+    const publisherCollection = client.db("newsPaper").collection("publisher");
+
+
+        // tittle related apis
+        app.post('/title', async (req, res) => {
+          const item = req.body;
+          const result = await articleCollection.insertOne(item);
+          res.send(result);
+        });
+
+        app.get('/title', async (req, res) => {
+          const result = await articleCollection.find().toArray();
+          res.send(result);
+        });
+    
+        // app.get('/title/:id', async (req, res) => {
+        //   const id = req.params.id;
+        //   const query = { _id: new ObjectId(id) }
+        //   const result = await articleCollection.findOne(query);
+        //   res.send(result);
+        // })
+    
+
+    
+        // app.patch('/title/:id', async (req, res) => {
+        //   const item = req.body;
+        //   const id = req.params.id;
+        //   const filter = { _id: new ObjectId(id) }
+        //   const updatedDoc = {
+        //     $set: {
+        //       name: item.name,
+        //       category: item.category,
+        //       price: item.price,
+        //       recipe: item.recipe,
+        //       image: item.image
+        //     }
+        //   }
+    
+        //   const result = await articleCollection.updateOne(filter, updatedDoc)
+        //   res.send(result);
+        // })
+    
+        // app.delete('/title/:id', async (req, res) => {
+        //   const id = req.params.id;
+        //   const query = { _id: new ObjectId(id) }
+        //   const result = await articleCollection.deleteOne(query);
+        //   res.send(result);
+        // })
+
+
+        // publisher collection
+        // app.post('/publisher', async (req, res) => {
+        //   const item = req.body;
+        //   const result = await publisherCollection.insertOne(item);
+        //   res.send(result);
+        // });
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
