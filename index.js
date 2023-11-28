9442fcf6a162c49d4120b80778a7359fe3b59f15
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-28 19:19:03 +0600

    view single article details

--- index.js
+++ index.js
@@ -90,13 +90,14 @@ async function run() {
     });
 
     // // get view details
-    // app.get('/title/viewDetails/:id',async(req,res) =>{
-    //   const id = req.params.id 
-    //   const query = {_id :new ObjectId(id)}
-    //   const result = await articleCollection.findOne(query)
-    //   res.send(result)
-    // })
-    // // 
+    app.get('/title/viewDetails/:id',async(req,res) =>{
+      const id = req.params.id 
+      const query = {_id :new ObjectId(id)}
+      const result = await articleCollection.findOne(query)
+      res.send(result)
+    })
+    
+    // 
     app.get('/title/update/:id',async(req,res) =>{
       const id = req.params.id 
       const query = {_id :new ObjectId(id)}
